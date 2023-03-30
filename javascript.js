647ef6bc680a8cdf5bb6b3db820d6aca66e34212
anand kumar <anand00kumar20@gmail.com>
2023-03-30 17:40:02 +0530

    popup event
    popup event

---
+++ javascript.js
@@ -0,0 +1,25 @@
+
+let clickButton = document.querySelector('.col1')
+
+clickButton.addEventListener('click', function () {
+  document.querySelector('.popup').style.display = 'flex'
+})
+let closeButton = document.querySelector('.button_close')
+
+
+closeButton.addEventListener('click', function () {
+  document.querySelector('.popup').style.display = 'none'
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
